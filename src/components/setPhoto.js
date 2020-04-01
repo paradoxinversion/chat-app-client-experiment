9f9abfbcad1a65ddd20f689cc36318f15c606b10
Jedai Saboteur <paradoxinversion@gmail.com>
2020-03-31 21:07:28 -0700

    Updated alt tag

--- src/components/setPhoto.js
+++ src/components/setPhoto.js
@@ -41,7 +41,7 @@ const SetPhoto = ({ clientUser }) => {
       {clientUser.profilePhotoURL && (
         <div>
           <p>Current Photo</p>
-          <img alt="Current user photo" src={clientUser.profilePhotoURL} />
+          <img alt="" src={clientUser.profilePhotoURL} />
         </div>
       )}
     </form>

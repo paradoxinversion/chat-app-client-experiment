a802de4a1607990bac7dc6b135e0257022fcb3a0
Jedai Saboteur <paradoxinversion@gmail.com>
2020-04-01 10:54:37 -0700

    Removed extra log

--- src/components/chatRoom.js
+++ src/components/chatRoom.js
@@ -40,7 +40,6 @@ class ChatRoom extends React.Component {
   }
 
   componentDidMount() {
-    console.log(process.env.REACT_APP_ADMIN_USER_ROLE);
     const serverURL = process.env.REACT_APP_SERVER_URL;
 
     const socketUrl = `${serverURL}`;
